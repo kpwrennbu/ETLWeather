f38cb485bdde49dc41e487d3f3d6f065ee7b3fd8
kpwrennbu <kpwrenn@bu.edu>
2025-03-14 18:04:06 -0400

    adds extraction to the ETL pipeline

--- dags/etlweather.py
+++ dags/etlweather.py
@@ -14,3 +14,30 @@ default_args={
     'owner': 'airflow', 
     'start_date': days_ago(1)
 }
+
+##DAG 
+with DAG(dag_id='weather_etl_pipeline', #gives dag an id
+         default_args=default_args, #gives it our args above
+         schedule_interval='@days', #runs daily
+         catchup=False
+         ) as dags: 
+    
+    @task() 
+    def extract_weather_data(): 
+        """Extract weather data from Open-Meteo API using Airflow Connection."""
+        
+        #Use HTTP Hook to get connection detail from Airflow connection
+        
+        http_hook = HTTPHook(http_conn_id=API_CONN_ID, method="GET")
+        
+        # Build the API endpoint 
+        # https://api.open-meteo.com/
+        endpoint=f'/v1/forecast?latitude={LATITUDE}&longitude={LONGITUDE}&current_weather=true'
+        
+        #Make the request via the HTTP Hook 
+        response = htt_hook.run(endpoint)
+        
+        if response.status_code == 200: 
+            return response.json() 
+        else: 
+            raise Exception(f"Failed to fetch weather data: {response.status_code}")

a2cbd138d232b314144769c2af1c507f382c9bdf
kpwrennbu <kpwrenn@bu.edu>
2025-03-14 17:51:49 -0400

    sets up params

--- dags/etlweather.py
+++ dags/etlweather.py
@@ -4,4 +4,13 @@ from airflow.providers.hooks.postgres import PostgresHook #For pushing our data 
 from airflow.decorators import task 
 from airflow.utils.dates import days_ago 
 
+#Latitude and Longitude for desired Location (Cohasset Mass)
+LATITUDE = '42.2417'
+LONGITUDE = '-70.8119'
+POSTGRES_CONN_ID='postrgres_default' #to connect to DB
+API_CONN_ID='open_meteo_id' #connection name so we keep a note of it 
 
+default_args={
+    'owner': 'airflow', 
+    'start_date': days_ago(1)
+}

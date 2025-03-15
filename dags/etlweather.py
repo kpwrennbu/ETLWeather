180c590a6445911975bf8420dc4ca683061c521c
kpwrennbu <kpwrenn@bu.edu>
2025-03-15 14:05:49 -0400

    adds working etl weather pipeline

--- dags/etlweather.py
+++ dags/etlweather.py
@@ -57,41 +57,38 @@ with DAG(dag_id='weather_etl_pipeline', #gives dag an id
         }
         return transformed_data
     @task 
-    def load_weather_data(transformed_data): 
-        #Load transformed data into PosgreSQL
+    def load_weather_data(transformed_data):
+        """Load transformed data into PostgreSQL."""
         pg_hook = PostgresHook(postgres_conn_id=POSTGRES_CONN_ID)
-        conn = pg_hook.get_conn() 
-        cursor = conn.cursor() 
-        
-        #Create table if it doesnt exist
-        cursor.execute(
-            """ 
-            CREATE TABLE IF NOT EXISTS weather_data ( 
-                latitude FLOAT,
-                longitude FLOAT, 
-                temperature FLOAT, 
-                windspeed FLOAT, 
-                winddirection FLOAT, 
-                weathercode INT
-                timestamp TIMESTAMP DEFAULT CURRENT TIMESTAMP   
-            );
-            """)
-        
-        #insert transformed data into the table 
-        cursor.execute(
-        """
+        conn = pg_hook.get_conn()
+        cursor = conn.cursor()
+
+        # Create table if it doesn't exist
+        cursor.execute("""
+        CREATE TABLE IF NOT EXISTS weather_data (
+            latitude FLOAT,
+            longitude FLOAT,
+            temperature FLOAT,
+            windspeed FLOAT,
+            winddirection FLOAT,
+            weathercode INT,
+            timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        );
+        """)
+
+        # Insert transformed data into the table
+        cursor.execute("""
         INSERT INTO weather_data (latitude, longitude, temperature, windspeed, winddirection, weathercode)
         VALUES (%s, %s, %s, %s, %s, %s)
-        """, ( 
-                transformed_data['latitude'], 
-                transformed_data['longitude'],
-                transformed_data['temperature'], 
-                transformed_data['windspeed'], 
-                transformed_data['winddirection'],
-                transformed_data['weathercode']  
-            )
-        )
-        
+        """, (
+            transformed_data['latitude'],
+            transformed_data['longitude'],
+            transformed_data['temperature'],
+            transformed_data['windspeed'],
+            transformed_data['winddirection'],
+            transformed_data['weathercode']
+        ))
+
         conn.commit()
         cursor.close()
         
